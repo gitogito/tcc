d576f57bd30b40dc2c287066f36d61878027feb9
ysk <ysk@cf19e7cc-152c-0410-a731-9059deedd3e6>
2010-05-24 14:37:46 +0000

    use variants instead of polymorphic variants
    git-svn-id: file:///Users/ysk/svn/tcc@211 cf19e7cc-152c-0410-a731-9059deedd3e6

--- trunk/tools/tccwrapper/syntax.ml
+++ trunk/tools/tccwrapper/syntax.ml
@@ -1,114 +1,111 @@
 (* All lists are LIFO (last in, first out). Take care of order *)
 
-type expr = [
-    `Plus of expr * expr
-  | `Minus of expr * expr
-  | `Multi of expr * expr
-  | `Div of expr * expr
-  | `Pow of expr * expr
-  | `Paren of expr
-  | `Neg of expr
-  | `Number of float
-  | `Var of string
-  | `Var_assign of string * expr
-]
-type var_assign    = [`Var_assign of string * expr]
-type point         = [`Point of expr * expr * expr]
-type vector        = [`Vector of expr * expr * expr]
-type point2d       = [`Point2d of expr * expr]
-type vector2d      = [`Vector2d of expr * expr]
-type point2d_list  = [`Nil | `Point2d_list of point2d * point2d_list]
-type vector2d_list = [`Nil | `Vector2d_list of vector2d * vector2d_list]
-type var_assigns   = [`Nil | `Var_assigns of var_assign * var_assigns]
-type world         = [`World of point * point * expr * expr * expr]
-type objs = [`Nil | `Objs of obj * objs]
-and obj = [
-    `Box of point * point
-  | `Rect of string * point * point2d
-  | `Triangle of string * point * point2d * point2d
-  | `Circle of string * point * expr
-  | `Circleperi of string * point * expr * expr * expr
-  | `Ellipse of string * point * expr * expr
-  | `Ellipseperi of string * point * expr * expr * expr * expr
-  | `Polygon of string * point * point2d_list
-  | `Line of string * point * point2d_list
-  | `Sweep of string * expr * obj
-  | `Objs of objs
-]
-type command = [
-    `Active of obj
-  | `Inactive of obj
-  | `Fix of expr * obj
-  | `Fixheat of expr * obj
-  | `Heat of expr * obj
-  | `Lambda of expr * obj
-]
-type commands = [`Nil | `Commands of command * commands]
+type 'a mylist = Nil | Cons of 'a * 'a mylist
 
-type input = [`Input of var_assigns * world * commands]
+type expr =
+    Plus of expr * expr
+  | Minus of expr * expr
+  | Multi of expr * expr
+  | Div of expr * expr
+  | Pow of expr * expr
+  | Paren of expr
+  | Neg of expr
+  | Number of float
+  | Var of string
+  | Var_assign_expr of string * expr
+
+type var_assign = Var_assign of string * expr
+type point      = Point of expr * expr * expr
+type vector     = Vector of expr * expr * expr
+type point2d    = Point2d of expr * expr
+type vector2d   = Vector2d of expr * expr
+type world      = World of point * point * expr * expr * expr
+
+type obj =
+    Box of point * point
+  | Rect of string * point * point2d
+  | Triangle of string * point * point2d * point2d
+  | Circle of string * point * expr
+  | Circleperi of string * point * expr * expr * expr
+  | Ellipse of string * point * expr * expr
+  | Ellipseperi of string * point * expr * expr * expr * expr
+  | Polygon of string * point * point2d mylist
+  | Line of string * point * point2d mylist
+  | Sweep of string * expr * obj
+  | Objs of obj mylist
+
+type command =
+    Active of obj
+  | Inactive of obj
+  | Fix of expr * obj
+  | Fixheat of expr * obj
+  | Heat of expr * obj
+  | Lambda of expr * obj
+
+type input = Input of var_assign mylist * world * command mylist
 
 (* return point from vector relative to the point *)
 let to_point point vector =
   match point with
-    `Point (px, py, pz) ->
+    Point (px, py, pz) ->
       match vector with
-        `Vector (vx, vy, vz) ->
-          `Point (
-            `Plus (px, vx),
-            `Plus (py, vy),
-            `Plus (pz, vz))
+        Vector (vx, vy, vz) ->
+          Point (
+            Plus (px, vx),
+            Plus (py, vy),
+            Plus (pz, vz))
 
 (* return point2d from vector2d relative to the point *)
 let to_point2d axis point vector2d =
   match point with
-    `Point (px, py, pz) ->
+    Point (px, py, pz) ->
       match vector2d with
-        `Vector2d (v1, v2) ->
+        Vector2d (v1, v2) ->
           match axis with
-            ":X" -> `Point2d (`Plus (py, v1), `Plus (pz, v2))
-          | ":Y" -> `Point2d (`Plus (pz, v1), `Plus (px, v2))
-          | ":Z" -> `Point2d (`Plus (px, v1), `Plus (py, v2))
+            ":X" -> Point2d (Plus (py, v1), Plus (pz, v2))
+          | ":Y" -> Point2d (Plus (pz, v1), Plus (px, v2))
+          | ":Z" -> Point2d (Plus (px, v1), Plus (py, v2))
           | _ -> failwith "unknown axis"
 
 (* return point2d list from vector2d_list relative to the point *)
 let rec to_point2d_list axis point vector2d_list =
   match vector2d_list with
-    `Nil -> `Nil
-  | `Vector2d_list (v, vl) ->
-      `Point2d_list (to_point2d axis point v, to_point2d_list axis point vl)
+    Nil -> Nil
+  | Cons (v, vl) ->
+      Cons (to_point2d axis point v, to_point2d_list axis point vl)
 
 let rec string_of_expr = function
-    `Plus (e1, e2) -> Printf.sprintf "%s + %s" (string_of_expr e1)
+    Plus (e1, e2) -> Printf.sprintf "%s + %s" (string_of_expr e1)
                         (string_of_expr e2)
-  | `Minus (e1, e2) -> Printf.sprintf "%s - %s" (string_of_expr e1)
+  | Minus (e1, e2) -> Printf.sprintf "%s - %s" (string_of_expr e1)
                          (string_of_expr e2)
-  | `Multi (e1, e2) -> Printf.sprintf "%s * %s" (string_of_expr e1)
+  | Multi (e1, e2) -> Printf.sprintf "%s * %s" (string_of_expr e1)
                          (string_of_expr e2)
-  | `Div (e1, e2) -> Printf.sprintf "%s / %s" (string_of_expr e1)
+  | Div (e1, e2) -> Printf.sprintf "%s / %s" (string_of_expr e1)
                        (string_of_expr e2)
-  | `Pow (e1, e2) -> Printf.sprintf "%s ** %s" (string_of_expr e1)
+  | Pow (e1, e2) -> Printf.sprintf "%s ** %s" (string_of_expr e1)
                        (string_of_expr e2)
-  | `Paren e -> Printf.sprintf "(%s)" (string_of_expr e)
-  | `Neg e -> Printf.sprintf "-%s" (string_of_expr e)
-  | `Number e -> Printf.sprintf "%g" e
-  | `Var e -> e
-  | `Var_assign (s, e) -> Printf.sprintf "%s = %s\n" s (string_of_expr e)
+  | Paren e -> Printf.sprintf "(%s)" (string_of_expr e)
+  | Neg e -> Printf.sprintf "-%s" (string_of_expr e)
+  | Number e -> Printf.sprintf "%g" e
+  | Var e -> e
+  | Var_assign_expr (s, e) -> Printf.sprintf "%s = %s\n" s (string_of_expr e)
 
 let string_of_point = function
-    `Point (x, y, z) ->
+    Point (x, y, z) ->
       Printf.sprintf "(%s, %s, %s)"
         (string_of_expr x)
         (string_of_expr y)
         (string_of_expr z)
 
 let string_of_point2d = function
-    `Point2d (p1, p2) ->
+    Point2d (p1, p2) ->
       Printf.sprintf "(%s, %s)"
         (string_of_expr p1)
         (string_of_expr p2)
 
 (* return "," in the beginning of string *)
 let rec string_of_point2d_list = function
-    `Nil -> ""
-  | `Point2d_list (p2d, p2dl) ->
+    Nil -> ""
+  | Cons (p2d, p2dl) ->
       string_of_point2d_list p2dl ^ ", " ^ string_of_point2d p2d
--- trunk/tools/tccwrapper/tccwrapper.ml
+++ trunk/tools/tccwrapper/tccwrapper.ml
@@ -1,15 +1,16 @@
 open Syntax
 
 let rec print_var_assigns = function
-    `Nil -> ()
-  | `Var_assigns (va, vas) ->
+    Nil -> ()
+  | Cons (va, vas) ->
       (* var_assigns has vars in reversed order, so va is printed after vas *)
       print_var_assigns vas;
-      match va with `Var_assign (s, e) ->
-        Printf.printf "%s = %s\n" s (string_of_expr e)
+      match va with
+        Var_assign (s, e) ->
+          Printf.printf "%s = %s\n" s (string_of_expr e)
 
 let print_world = function
-    `World (pnt1, pnt2, dx, dy, dz) ->
+    World (pnt1, pnt2, dx, dy, dz) ->
       print_endline "--- world";
       print_string (string_of_point pnt1);
       print_string ", ";
@@ -20,76 +21,76 @@ let print_world = function
         (string_of_expr dz)
 
 let rec print_objs = function
-    `Nil -> ()
-  | `Objs (obj, objs) ->
+    Nil -> ()
+  | Cons (obj, objs) ->
       print_objs objs;
       print_obj obj
 
 and print_obj = function
-    `Box (p1, p2) ->
+    Box (p1, p2) ->
       Printf.printf "box %s, %s\n" (string_of_point p1) (string_of_point p2)
-  | `Rect (s, p, p2d) ->
+  | Rect (s, p, p2d) ->
       Printf.printf "rect %s, %s, %s\n" s (string_of_point p)
         (string_of_point2d p2d)
-  | `Triangle (s, p, p2d1, p2d2) ->
+  | Triangle (s, p, p2d1, p2d2) ->
       Printf.printf "triangle %s, %s, %s, %s\n" s (string_of_point p)
         (string_of_point2d p2d1) (string_of_point2d p2d2)
-  | `Circle (s, p, e) ->
+  | Circle (s, p, e) ->
       Printf.printf "circle %s, %s, %s\n" s (string_of_point p)
         (string_of_expr e)
-  | `Circleperi (s, p, e1, e2, e3) ->
+  | Circleperi (s, p, e1, e2, e3) ->
       Printf.printf "circleperi %s, %s, %s, %s, %s\n" s (string_of_point p)
         (string_of_expr e1) (string_of_expr e2) (string_of_expr e3)
-  | `Ellipse (s, p, e1, e2) ->
+  | Ellipse (s, p, e1, e2) ->
       Printf.printf "ellipse %s, %s, %s, %s\n" s (string_of_point p)
         (string_of_expr e1) (string_of_expr e2)
-  | `Ellipseperi (s, p, e1, e2, e3, e4) ->
+  | Ellipseperi (s, p, e1, e2, e3, e4) ->
       Printf.printf "ellipseperi %s, %s, %s, %s, %s, %s\n" s (string_of_point p)
         (string_of_expr e1) (string_of_expr e2)
         (string_of_expr e3) (string_of_expr e4)
-  | `Polygon (s, p, p2d_list) ->
+  | Polygon (s, p, p2d_list) ->
       Printf.printf "polygon %s, %s %s\n" s (string_of_point p)
         (string_of_point2d_list p2d_list)
-  | `Line (s, p, p2d_list) ->
+  | Line (s, p, p2d_list) ->
       Printf.printf "line %s, %s %s\n" s (string_of_point p)
         (string_of_point2d_list p2d_list)
-  | `Sweep (s, e, obj) ->
+  | Sweep (s, e, obj) ->
       Printf.printf "sweep %s, %s, " s (string_of_expr e);
       print_obj obj
-  | `Objs objs ->
+  | Objs objs ->
       print_endline "[";
       print_objs objs;
       print_endline "]"
 
 let print_command = function
-    `Active obj ->
+    Active obj ->
       print_endline "--- active";
       print_obj obj
-  | `Inactive obj ->
+  | Inactive obj ->
       print_endline "--- inactive";
       print_obj obj
-  | `Fix (expr, obj) ->
+  | Fix (expr, obj) ->
       Printf.printf "--- fix %s\n" (string_of_expr expr);
       print_obj obj
-  | `Fixheat (expr, obj) ->
+  | Fixheat (expr, obj) ->
       Printf.printf "--- fix heat %s\n" (string_of_expr expr);
       print_obj obj
-  | `Heat (expr, obj) ->
+  | Heat (expr, obj) ->
       Printf.printf "--- heat %s\n" (string_of_expr expr);
       print_obj obj
-  | `Lambda (expr, obj) ->
+  | Lambda (expr, obj) ->
       Printf.printf "--- lambda %s\n" (string_of_expr expr);
       print_obj obj
 
 let rec print_commands = function
-    `Nil -> ()
-  | `Commands (a, l) ->
+    Nil -> ()
+  | Cons (a, l) ->
       print_command a;
       print_newline ();
       print_commands l
 
 let print_input = function
-    `Input (var_assigns, world, commands) ->
+    Input (var_assigns, world, commands) ->
       print_var_assigns var_assigns;
       print_newline ();
 
@@ -101,4 +102,4 @@ let print_input = function
 let () =
   let lexbuf = Lexing.from_channel stdin in
   let input = Parser.input Lexer.token lexbuf in
-    print_input input
+  print_input input
